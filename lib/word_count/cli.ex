a1193e9fb657c4f4ebc7527291c6653542f159ee
josemimbre <josemimbre@hotmail.es>
2022-01-01 18:48:28 +0100

    merge init function

--- lib/word_count/cli.ex
+++ lib/word_count/cli.ex
@@ -1,11 +1,14 @@
 defmodule WordCount.CLI do
-  def main([]) do
-    stdio = IO.stream(:stdio, :line)
-    WordCount.process_streams([stdio])
-  end
-
   def main(args) do
-    streams = for file <- args, do: File.stream!(file, read_ahead: 100_000)
-    WordCount.process_streams(streams)
+    case args do
+      [] ->
+        stdio = IO.stream(:stdio, :line)
+        streams = [stdio]
+        WordCount.process_streams(streams)
+
+      _ ->
+        streams = for file <- args, do: File.stream!(file, read_ahead: 100_000)
+        WordCount.process_streams(streams)
+    end
   end
 end
